ee551244d48e6bdeb97bfc82af411c3c1831991b
POrGSrPOGS <POrGSrPOGS@gmail.com>
2026-08-05 09:09:16 +0200

    edited home path to be /home

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
